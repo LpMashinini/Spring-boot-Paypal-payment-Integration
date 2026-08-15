532939fb138a6582baf3643f151be0c697c289e1
LpMashinini <lebogangmatshinini@gmail.com>
2026-08-15 16:09:28 +0200

    first commit

---
+++ PayPal-integration/src/test/java/com/payment/PayPal_integration/PayPalIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.PayPal_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayPalIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
